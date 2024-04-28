fe70c3dc42ab2930daa195d7a7e2b1e44f59b79d
Gogaturadze <gogaturadze@gmail.com>
2024-04-28 12:02:29 +0400

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'HomeWork37';
+  title = 'Fibonaci';
+
+  generateFibonacci() {
+    let exportNum: any = document.getElementById('exportNum');
+    let numValue: number = parseInt(exportNum.value);
+    let output: any = document.getElementById('output');
+    let nums: number[] = [];
+    let num1: number = 0;
+    let num2: number = 1;
+    let nextNum: number;
+    for (let i = 0; i <= numValue; i++) {
+      nums.push(num1);
+      nextNum = num1 + num2;
+      num1 = num2;
+      num2 = nextNum;
+    }
+    output.innerHTML = nums.join(', ');
+  }
 }
